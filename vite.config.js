4097838ff3997f2c06ddb006ec3450a2ced00712
Abir Afridi <abirafridi87@gmail.com>
2025-02-08 07:41:14 +0500

    react wather app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wather-app-on-react-.wow.git',
+  base: '/wather-app-with-react',
 })
